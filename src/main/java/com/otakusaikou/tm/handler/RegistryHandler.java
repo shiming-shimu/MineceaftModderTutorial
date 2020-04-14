c306f3dafc931bac384947b302996a8c3f263e48
shiming-shimu <62317052+shiming-shimu@users.noreply.github.com>
2020-04-14 10:07:54 +0800

    Revert "It's e6 commit"
    This reverts commit 47f4c8bd0987e921790f3c193194467a7c93f655.

--- src/main/java/com/otakusaikou/tm/handler/RegistryHandler.java
+++ src/main/java/com/otakusaikou/tm/handler/RegistryHandler.java
@@ -1,16 +1,12 @@
 package com.otakusaikou.tm.handler;
 
-import com.otakusaikou.tm.Init.ModBlocks;
 import com.otakusaikou.tm.Init.ModItems;
 import com.otakusaikou.tm.interfaces.IHasModel;
-import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-
-import java.util.List;
 
 @EventBusSubscriber
 public class RegistryHandler {
@@ -19,20 +15,10 @@ public class RegistryHandler {
         event.getRegistry().registerAll(ModItems.ITEMS.toArray(new Item[0]));
     }
     @SubscribeEvent
-    public static void onBlockRegistery(RegistryEvent.Register<Block> event){
-        event.getRegistry().registerAll(ModBlocks.BLOCKS.toArray(new Block[0]));
-    }
-
-    @SubscribeEvent
     public static void onModelRegistery(ModelRegistryEvent event){
-        modelRegisteryHelper(ModItems.ITEMS);
-        modelRegisteryHelper(ModBlocks.BLOCKS);
-    }
-
-    private static <T> void modelRegisteryHelper(List<T> list){
-        for(T element : list){
-            if(element instanceof IHasModel){
-                ((IHasModel) element).registryItemRender();
+        for(Item item : ModItems.ITEMS){
+            if(item instanceof IHasModel){
+                ((IHasModel) item).registryItemRender();
             }
         }
     }
